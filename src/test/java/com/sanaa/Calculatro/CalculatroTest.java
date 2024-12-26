a70ee65ccbe39ea5448741e6ecba50bc3eb0d18b
sanaa <fanirisanaa@gmail.com>
2024-12-26 16:37:42 +0100

    beforeeach et aftereach

--- src/test/java/com/sanaa/Calculatro/CalculatroTest.java
+++ src/test/java/com/sanaa/Calculatro/CalculatroTest.java
@@ -1,19 +1,29 @@
 package com.sanaa.Calculatro;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 public class CalculatroTest {
+    Calculatro ca;
+    @BeforeEach
+    public void setup() {
+    ca = new Calculatro();
+    System.out.println("Calculatrice utilisation");}
+    @AfterEach
+    public void tearDown() {
+    System.out.println("nettoyage");
+    ca=null;}
     @Test
     public void testAddition() {
-        Calculatro calculator = new Calculatro();
-        int result = calculator.add(2, 3);
-        Assertions.assertThat(result).isEqualTo(5);
+        int result = ca.add(2, 3);
+        Assertions.assertEquals(5,result,"2+3 doit etre egal a 5");
     }
     @Test
     public void testdivision()
     {
         Calculatro c=new Calculatro();
         int r=c.divide(4,2);
-        Assertions.assertThat(r).isEqualTo(2);
+        Assertions.assertEquals(2,r,"4/2 doit etre egal a 2");
 
     }
 }
