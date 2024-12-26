dce510eefa0de9b92537b7d7163412ff1a7a03bd
sanaa <fanirisanaa@gmail.com>
2024-12-26 16:48:13 +0100

    static

--- src/main/java/com/sanaa/Calculatro/Calculatro.java
+++ src/main/java/com/sanaa/Calculatro/Calculatro.java
@@ -1,11 +1,11 @@
 package com.sanaa.Calculatro;
 
 public class Calculatro {
-    public int add(int a, int b)
+    public static int add(int a, int b)
     {
         return a+b;
     }
-    public int divide(int c, int d)
+    public static int divide(int c, int d)
     {
         return c/d;
     }
