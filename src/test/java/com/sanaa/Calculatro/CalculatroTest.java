5f6ceb6840da494f36840ab53a8bb45c75e62504
sanaa <fanirisanaa@gmail.com>
2024-12-26 16:09:29 +0100

    abc

--- src/test/java/com/sanaa/Calculatro/CalculatroTest.java
+++ src/test/java/com/sanaa/Calculatro/CalculatroTest.java
@@ -1,4 +1,19 @@
 package com.sanaa.Calculatro;
-
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 public class CalculatroTest {
+    @Test
+    public void testAddition() {
+        Calculatro calculator = new Calculatro();
+        int result = calculator.add(2, 3);
+        Assertions.assertThat(result).isEqualTo(5);
+    }
+    @Test
+    public void testdivision()
+    {
+        Calculatro c=new Calculatro();
+        int r=c.divide(4,2);
+        Assertions.assertThat(r).isEqualTo(2);
+
+    }
 }
