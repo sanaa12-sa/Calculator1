744f2848b467867788e5506337efacdf809544bf
sanaa <fanirisanaa@gmail.com>
2024-12-26 17:21:43 +0100

    parametrized

--- src/test/java/com/sanaa/Calculatro/CalculatroTest.java
+++ src/test/java/com/sanaa/Calculatro/CalculatroTest.java
@@ -1,17 +1,32 @@
 package com.sanaa.Calculatro;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 public class CalculatroTest {
-    @Test
-    public void testAddition() {
-        int result = Calculatro.add(2, 3);
-        Assertions.assertEquals(5,result,"2+3 doit etre egal a 5");
+    @ParameterizedTest
+    @CsvSource(
+            {
+                    "1,1,2",
+                    "2,3,5",
+                    "4,4,8"
+            }
+    )
+    public void testAddition(int x, int y, int z) {
+        int result = Calculatro.add(x, y);
+        Assertions.assertEquals(z,result,"2+3 doit etre egal a 5");
     }
-    @Test
-    public void testdivision()
+    @ParameterizedTest
+    @CsvSource(
+            {
+                    "1,1,1",
+                    "4,2,2",
+                    "4,4,1"
+            }
+    )
+    public void testdivision(int a,int b, int d)
     {
-        int r=Calculatro.divide(4,2);
-        Assertions.assertEquals(2,r,"4/2 doit etre egal a 2");
+        int r=Calculatro.divide(a,b);
+        Assertions.assertEquals(d,r,"4/2 doit etre egal a 2");
 
     }
 }
